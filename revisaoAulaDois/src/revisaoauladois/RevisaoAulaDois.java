a222d5c40a1ab5cf4c2f4c28627b49a5db981082
GC-Aula <GC-Aula@GC-Aula-PC>
2019-06-06 20:40:28 -0300

    Commit Revisao Marcos Flavio

---
+++ revisaoAulaDois/src/revisaoauladois/RevisaoAulaDois.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisaoauladois;
+
+/**
+ *
+ * @author GC-Aula
+ */
+public class RevisaoAulaDois {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloPeople ola = new HelloPeople();
+        System.out.println(ola.printNome("Marcos"));
+    }
+    
+}
